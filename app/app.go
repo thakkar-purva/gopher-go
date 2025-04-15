31bb9dda15996a68edfee50dfef666a19db960c8
Purva Thakkar <pjthakkar3192@gmail.com>
2025-04-14 18:19:39 -0600

    import custom packages

--- app/app.go
+++ app/app.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gopher-go/app/tutorials" // Import the tutorials package
+)
 
 // entry point for go project  NOTE: has to be main() method in main package
 // file name can be anything.
 func main() {
 	fmt.Println("Hello World !!")
+	tutorials.PrintsTutorial() // Call the exported function from the tutorials package
 }
