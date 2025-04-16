1f49e949a22fe4e5db3f242e5044847cd42ef57d
Purva Thakkar <pjthakkar3192@gmail.com>
2025-04-16 15:10:02 -0600

    inpouts package

--- app/tutorials/inputs.go
+++ app/tutorials/inputs.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+// The above code uses the bufio package to read input from the console.
+// It creates a new scanner that reads from os.Stdin (the standard input stream).
 func PrintsInput() {
 	fmt.Println("What is your name?")
 	scanner := bufio.NewScanner(os.Stdin)
@@ -13,6 +15,3 @@ func PrintsInput() {
 	name := scanner.Text()
 	fmt.Println("Hello", name)
 }
-
-// The above code uses the bufio package to read input from the console.
-// It creates a new scanner that reads from os.Stdin (the standard input stream).
